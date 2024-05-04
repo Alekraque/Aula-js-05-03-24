a2cd18f29c96ec9f6c46c57453b28e6fa39f80e5
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2024-05-03 22:37:18 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// function somar(a, b, c){
+//     return a+b
+// }
+// function printarNaTela(){
+//     const resultado = somar(5, 7)
+//     console.log(resultado)
+// }
+
+// const pessoas = ['a', 'b', 'c']
+// console.log(pessoas.length)
+
+
+function escolhaPc(){
+    const opcao = ['pedra', 'papel', 'tesoura']
+    const numRamdom = (Math.floor(Math.random() * 3))
+    return opcao[numRamdom]
+}
+
+function jogar(opcaoUsuario){
+
+   const opcaoComp = escolhaPc()
+
+    if (opcaoUsuario === opcaoComp ){
+
+    }
+}
