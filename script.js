59ffe2e9660b409ec400de645578c30ba94a4af6
Alex Ribeiro Maia <161780361+Alekraque@users.noreply.github.com>
2024-05-04 04:04:39 -0300

    exercicio resolvido

--- script.js
+++ script.js
@@ -9,18 +9,26 @@
 // const pessoas = ['a', 'b', 'c']
 // console.log(pessoas.length)
 
-
 function escolhaPc(){
     const opcao = ['pedra', 'papel', 'tesoura']
     const numRamdom = (Math.floor(Math.random() * 3))
     return opcao[numRamdom]
 }
-
 function jogar(opcaoUsuario){
 
-   const opcaoComp = escolhaPc()
-
+    let resultado = ''
+    const opcaoComp = escolhaPc()
     if (opcaoUsuario === opcaoComp ){
-
+        resultado = 'empate'
+        console.log(resultado)
+    } else if(
+        (opcaoComp === 'pedra' && opcaoUsuario === 'papel')||
+        (opcaoComp === 'papel' && opcaoUsuario === 'tesoura')||
+        (opcaoComp === 'tesoura' && opcaoUsuario === 'pedra')
+    ){
+        resultado = 'Ganhou, maluco!'
+    } else{
+        resultado = 'Perdeu, otário!'
     }
+    document.getElementById('resultado').innerText = 'você escolheu ' + opcaoUsuario + ' e o computador escolheu ' + opcaoComp + '. O resultado foi: ' + resultado
 }
